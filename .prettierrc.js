e78772e766917c354ff2db255743d0614759300e
Jim <ajcastelfaller@gmail.com>
2023-03-20 10:50:27 +0800

    initial amplify setup

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 100,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  arrowParens: 'avoid',
+  insertPragma: false
+};
